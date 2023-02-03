f384de150ff81f134dd8ad15e74a323651549f85
jo0713ji <so0627suke@gmail.com>
2023-02-03 17:22:43 +0900

    Add:フォローに関するユーザー情報を取得

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,13 @@ class User < ApplicationRecord
   has_many :notification_timings, through: :user_notification_timings
   # フォローする側から中間テーブルへのアソシエーション
   has_many :relationships, foreign_key: :following_id
+  # フォローする側からフォローされたユーザを取得する
+  has_many :followings, through: :relationships, source: :follower
   # フォローされる側から中間テーブルへのアソシエーション
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # フォローされる側からフォローしているユーザを取得する
+  has_many :followers, through: :reverse_of_relationships, source: :following
+
   has_one_attached :avatar
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
